317eedfa7d033b254cd8beeb5574e0d7231410b7
Lifei Li <llf17@uw.edu>
2014-10-14 00:53:14 -0700

    start exposing RESTful APIs

---
+++ controller/profile.js
@@ -0,0 +1,10 @@
+exports.getweight = function (req, res){
+    if(req.params.userid == 1){
+        res.send('hi');
+    }
+};
+exports.updateweight = function (req, res){
+
+};
+
+module.exports = exports;
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var Schema = mongoose.Schema;
+//Http and Https
+var fs = require('fs');
+var http = require('http');
+//var https = require('https');
+
+//Modules
+var profile = require('./controller/profile');
+
+//Database
+mongoose.connect = ('mongodb://trackhealth:trackhealth@ds041160.mongolab.com:41160/trackhealth');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+//Model
+var healthData = new Schema({
+    userid: String,
+    wg: [{ date:Date, weight:Number}]
+});
+//APIs
+app.get('/api/users/:userid/weight',profile.getweight);
+app.post('/api/users/:userid/weight',profile.updateweight);
+//Setting up link
+var httpServer = http.createServer(app);
+httpServer.listen(3000);
